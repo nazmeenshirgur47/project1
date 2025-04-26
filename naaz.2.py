55fa88cd033555ebde1b98171667b138595b0119
nazmeenshirgur47 <nazmeennazeerahmedshirgur@gmail.com>
2025-04-26 12:47:23 +0530

    Add files via upload

---
+++ naaz.2.py
@@ -0,0 +1,13 @@
+print("list")
+l1=[1,2,"ABC",3"XYZ"2,3]
+print(1)
+print("Dictionary")
+D1={"A":134"B":266"C":343}
+print(D1)
+print("Tuples")
+T1=(10,20,30,40,50,40)
+print(T1)
+print("sets")
+S1={10,20,30,40,10,30,40,20,50,50}
+print(S1)
+      
